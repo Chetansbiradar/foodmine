2551a04b261e7f003bf45859ae737a30b5709b1b
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-04-16 23:45:57 +0530

    home component render items

---
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FoodService } from 'src/app/services/food.service';
+import { Food } from 'src/app/shared/models/foods';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  foods: Food[] = [];
+  constructor(private foodService: FoodService, activatedRoute:ActivatedRoute) {
+    activatedRoute.params.subscribe(params=>{
+      if(params.searchTerm){
+        this.foods = foodService.getFoodBySearch(params.searchTerm);
+      }else{
+        this.foods = foodService.getAll();
+      }
+    })
+    // this.foods = foodService.getAll();
+  }
+}
