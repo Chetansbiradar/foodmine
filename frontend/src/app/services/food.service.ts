6ad2542053e18866edaca324e09a230654c45cf5
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-04-19 23:33:03 +0530

    food service using httpclient api call

--- frontend/src/app/services/food.service.ts
+++ frontend/src/app/services/food.service.ts
@@ -1,19 +1,37 @@
 import { Injectable } from '@angular/core';
-import { Food } from '../shared/models/foods';
-import { sampleFoods } from 'src/data';
+import { Food } from '../shared/models/Food';
+import { Tag } from '../shared/models/Tag';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { FOODS_BY_SEARCH_URL, FOODS_BY_TAG_URL, FOODS_TAGS_URL, FOODS_URL, FOOD_BY_ID_URL } from '../shared/constants/urls';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FoodService {
 
-  constructor() {
-  }
-  getAll():Food[]{
-    return sampleFoods
+  constructor(private http:HttpClient) {
   }
 
-  getFoodBySearch(search:string):Food[]{
-    return sampleFoods.filter(food=>food.name.toLowerCase().includes(search.toLowerCase()))
+  getAll(): Observable<Food[]>{
+    return this.http.get<Food[]>(FOODS_URL);
+  }
+
+  getFoodBySearch(search:string):Observable<Food[]>{
+    return this.http.get<Food[]>(FOODS_BY_SEARCH_URL+search)
+  }
+
+  getAllTags():Observable<Tag[]>{
+    return this.http.get<Tag[]>(FOODS_TAGS_URL);
+  }
+
+  getFoodsByTag(tag:string):Observable<Food[]>{
+    return tag==='All'?
+    this.getAll():
+    this.http.get<Food[]>(FOODS_BY_TAG_URL+tag);
+  }
+
+  getFoodById(id:string):Observable<Food>{
+    return this.http.get<Food>(FOOD_BY_ID_URL+id);
   }
 }
