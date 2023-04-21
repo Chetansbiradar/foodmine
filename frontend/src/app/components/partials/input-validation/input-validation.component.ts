cba01f0f742d909c69c79a06b03cefc977026a1a
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-04-21 16:47:52 +0530

    input validation component

---
+++ frontend/src/app/components/partials/input-validation/input-validation.component.ts
@@ -0,0 +1,45 @@
+import { Component,Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+
+
+const VALIDATORS_MESSAGE:any={
+  required:'This field is required',
+  email:'Please enter a valid email',
+
+}
+@Component({
+  selector: 'input-validation',
+  templateUrl: './input-validation.component.html',
+  styleUrls: ['./input-validation.component.css']
+})
+export class InputValidationComponent implements OnInit,OnChanges {
+  @Input() control!:AbstractControl;
+  @Input() showErrorsWhen:boolean=true;
+  errorMessages:string[]=[];
+
+  ngOnChanges(changes: SimpleChanges):void{
+    this.checkValidation();
+  }
+
+  ngOnInit():void{
+    this.control.statusChanges.subscribe(()=>{
+      this.checkValidation();
+    });
+
+    this.control.valueChanges.subscribe(()=>{
+      this.checkValidation();
+    });
+  }
+
+  checkValidation(){
+    const errors=this.control.errors;
+    if(!errors){
+      this.errorMessages = [];
+      return;
+    }
+
+    const errorKeys = Object.keys(errors);
+    this.errorMessages = errorKeys.map(key => VALIDATORS_MESSAGE[key]);
+
+  }
+}
